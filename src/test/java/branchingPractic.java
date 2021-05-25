9cab198116a9777fadba7aa94463dbb0bdb98497
Effyyulghun <82982417+Effyyulghun@users.noreply.github.com>
2021-05-25 16:26:38 -0700

    new project start working

--- src/test/java/branchingPractic.java
+++ src/test/java/branchingPractic.java
@@ -1,2 +1,7 @@
 public class branchingPractic {
+    //creat new project
+    // enabled version control
+    //added.gitignore with .ide .target folder and .iml file
+    //made initial commit by keeping track of all files
+    //shared project on github
 }
